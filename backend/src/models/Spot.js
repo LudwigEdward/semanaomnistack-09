21db46d8ed902e38b50f56cba6f5f412d341c8fe
Luís Eduardo <le.barros20166@hotmail.com>
2019-10-03 19:55:00 -0400

    Implementado front-end e implementando novas funcionalidades no back-end

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
-const SpotSchema = mongoose.Schema({
-  thumbnail: String,
-  company: String,
-  techs: [String],
-  price: Number,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+const SpotSchema = mongoose.Schema(
+  {
+    thumbnail: String,
+    company: String,
+    techs: [String],
+    price: Number,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model("Spot", SpotSchema);
